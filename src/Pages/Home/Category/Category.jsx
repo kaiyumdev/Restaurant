82fae9088932d1d3c2b9ba9a2f80c977f9ad6abd
kaiyumdev <afrankaiyum34@gmail.com>
2024-09-03 20:26:06 +0600

    feat(category): complete category slider

--- src/Pages/Home/Category/Category.jsx
+++ src/Pages/Home/Category/Category.jsx
@@ -1,13 +1,64 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import slide1 from "../../../assets/home/slide1.jpg";
+import slide2 from "../../../assets/home/slide2.jpg";
+import slide3 from "../../../assets/home/slide3.jpg";
+import slide4 from "../../../assets/home/slide4.jpg";
+import slide5 from "../../../assets/home/slide5.jpg";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 
 const Category = () => {
   return (
-    <div>
+    <section>
       <SectionTitle
-        heading="Order Online"
-        subHeading="From 11:00am to 10:00pm"
+        heading={"Order Online"}
+        subHeading={"From 11:00am to 10:00pm"}
       ></SectionTitle>
-    </div>
+      <Swiper
+        slidesPerView={4}
+        spaceBetween={30}
+        centeredSlides={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper mb-24"
+      >
+        <SwiperSlide>
+          <img src={slide1}></img>
+          <h3 className="text-4xl uppercase text-center -mt-20 text-white">
+            Salads
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide2}></img>
+          <h3 className="text-4xl uppercase text-center -mt-20 text-white">
+            Pizzas
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide3}></img>
+          <h3 className="text-4xl uppercase text-center -mt-20 text-white">
+            Soups
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide4}></img>
+          <h3 className="text-4xl uppercase text-center -mt-20 text-white">
+            Desserts
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide5}></img>
+          <h3 className="text-4xl uppercase text-center -mt-20 text-white">
+            Salads
+          </h3>
+        </SwiperSlide>
+      </Swiper>
+    </section>
   );
 };
 
