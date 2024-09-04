2d668a8388b79a7587bd50da0fba5d79f68e68fe
kaiyumdev <afrankaiyum34@gmail.com>
2024-09-04 10:44:52 +0600

    feat(menuItem): complete menuItem successfully

--- src/Shared/MenuItem/MenuItem.jsx
+++ src/Shared/MenuItem/MenuItem.jsx
@@ -3,20 +3,18 @@
 const MenuItem = ({ item }) => {
   const { name, image, recipe, price } = item;
   return (
-    <div>
-      <div className="flex">
-        <img
-          style={{ borderRadius: "0 200px 200px 200px" }}
-          className="w-16 h-16"
-          src={image}
-          alt={image}
-        />
-        <div>
-          <h4 className="text-xl font-bold">{name}</h4>
-          <p>{recipe}</p>
-        </div>
-        <span className="text-yellow-600">{price}</span>
+    <div className="flex items-center gap-4">
+      <img
+        style={{ borderRadius: "0 200px 200px 200px" }}
+        className="w-16 h-16"
+        src={image}
+        alt={image}
+      />
+      <div>
+        <h4 className="text-xl font-bold">{name}</h4>
+        <p>{recipe}</p>
       </div>
+      <span className="text-yellow-600">{price}</span>
     </div>
   );
 };
