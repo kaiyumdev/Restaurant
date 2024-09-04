03f0f8dc1048ba4738e113c7731e50f7625ff8d1
kaiyumdev <afrankaiyum34@gmail.com>
2024-09-04 14:46:25 +0600

    feat(coverImage): implements cover image for menu successuflly

--- src/Pages/Menu/Menu.jsx
+++ src/Pages/Menu/Menu.jsx
@@ -1,9 +1,17 @@
 import Cover from "../../Shared/Cover/Cover";
+import useMenu from "../../hooks/useMenu";
+import menuImg from "../../assets/menu/banner3.jpg";
 
 const Menu = () => {
+  const [menu] = useMenu();
+  const desserts = menu.filter((item) => item.category === "dessert");
+  const salads = menu.filter((item) => item.category === "salad");
+  const pizzas = menu.filter((item) => item.category === "pizza");
+  const offered = menu.filter((item) => (item.category = "offered"));
+  const soups = menu.filter((item) => item.category === "soup");
   return (
     <div>
-      <Cover></Cover>
+      <Cover img={menuImg} title="Our Menu"></Cover>
     </div>
   );
 };
