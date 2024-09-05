e1cb08a0df25f3b897f630fda047368d0976e73c
kaiyumdev <afrankaiyum34@gmail.com>
2024-09-05 12:26:13 +0600

    feat(pagination): generate paginatin function to show six items at a page

--- src/Pages/Order/OrderTab/OrderTab.jsx
+++ src/Pages/Order/OrderTab/OrderTab.jsx
@@ -1,22 +1,41 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react";
 import FoodCard from "../../../components/FoodCard/FoodCard";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/pagination";
-import { Pagination } from "swiper/modules";
 
 const OrderTab = ({ items }) => {
+  const [currentPage, setCurrentPage] = useState(0);
+  const itemsPerPage = 6;
+
+  // Get the items for the current page
+  const currentItems = items.slice(
+    currentPage * itemsPerPage,
+    (currentPage + 1) * itemsPerPage
+  );
+
   return (
     <div>
-      <Swiper pagination={true} modules={[Pagination]} className="mySwiper">
-        <SwiperSlide>
-          <div className="grid md:grid-cols-3 gap-10">
-            {items.map((item, index) => (
-              <FoodCard key={index} item={item}></FoodCard>
-            ))}
-          </div>
-        </SwiperSlide>
-      </Swiper>
+      <div className="grid md:grid-cols-3 gap-10">
+        {currentItems.map((item, index) => (
+          <FoodCard key={index} item={item}></FoodCard>
+        ))}
+      </div>
+      <div className="flex justify-center mt-8">
+        {/* Render pagination buttons */}
+        {Array.from(
+          { length: Math.ceil(items.length / itemsPerPage) },
+          (_, index) => (
+            <button
+              key={index}
+              onClick={() => setCurrentPage(index)}
+              className={`btn mx-1 ${
+                currentPage === index ? "btn-primary" : "btn-outline"
+              }`}
+            >
+              {index + 1}
+            </button>
+          )
+        )}
+      </div>
     </div>
   );
 };
