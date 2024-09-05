65cf2f84f00da3ae4e92ddab9085f757cf5cde0a
kaiyumdev <afrankaiyum34@gmail.com>
2024-09-05 21:06:07 +0600

    feat(routes): create login & signup routes

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,6 +3,8 @@ import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home/Home";
 import Menu from "../Pages/Menu/Menu/Menu";
 import Order from "../Pages/Order/Order/Order";
+import Login from "../Shared/Login/Login";
+import SignUp from "../Shared/SignUp/SignUp";
 
 export const router = createBrowserRouter([
   {
@@ -20,6 +22,14 @@ export const router = createBrowserRouter([
       {
         path: "order/:category",
         element: <Order></Order>,
+      },
+      {
+        path: "login",
+        element: <Login></Login>,
+      },
+      {
+        path: "signup",
+        element: <SignUp></SignUp>,
       },
     ],
   },
