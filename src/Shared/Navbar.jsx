447d63c64454f261803dcbce1bfe7874630f738a
kaiyumdev <afrankaiyum34@gmail.com>
2024-09-07 12:18:50 +0600

    feat(navbar): complete navbar for restaurant project

--- src/Shared/Navbar.jsx
+++ src/Shared/Navbar.jsx
@@ -10,25 +10,27 @@ const Navbar = () => {
   };
   const navOptions = (
     <>
-      {" "}
-      <li>
+      <li className="flex items-center justify-center">
         <Link to="/">Home</Link>
       </li>
-      <li>
+      <li className="flex items-center justify-center">
         <Link to="/menu">Menu</Link>
       </li>
-      <li>
+      <li className="flex items-center justify-center">
         <Link to="/order/salad">Order</Link>
       </li>
-      <li>
+      <li className="flex items-center justify-center">
         <Link to="/signup">SignUp</Link>
       </li>
-      <li>
-        <Link to="/secret">Secret</Link>
-      </li>
-      <li>
-        <a>Dashboard</a>
-      </li>
+      {/* <li className="flex items-center justify-center">
+        <Link to="/dashboard/cart">
+          <button className="btn flex items-center justify-center">
+            <FaShoppingCart />
+            <div className="badge badge-secondary">+{cart?.length}</div>
+          </button>
+        </Link>
+      </li> */}
+
       {user ? (
         <li className="flex items-center justify-center">
           <button onClick={handleLogOut} className="btn btn-ghost">
@@ -42,17 +44,17 @@ const Navbar = () => {
           </button>
         </li>
       )}
-      <li>
-        {" "}
-        <span>{user && user.displayName}</span>
-      </li>
     </>
   );
   return (
-    <div className="navbar fixed z-10 bg-opacity-30 text-white bg-black">
+    <div className="navbar fixed z-10 bg-opacity-30 max-w-screen-xl bg-black text-white">
       <div className="navbar-start">
         <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost lg:hidden flex items-center justify-center"
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-5 w-5"
@@ -75,13 +77,20 @@ const Navbar = () => {
             {navOptions}
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">Restaurant</a>
+        <Link
+          to="/"
+          className="btn btn-ghost text-xl flex items-center justify-center"
+        >
+          Bistro Boss
+        </Link>
       </div>
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">{navOptions}</ul>
+        <ul className="menu menu-horizontal px-1 flex items-center justify-center">
+          {navOptions}
+        </ul>
       </div>
       <div className="navbar-end">
-        <a className="btn">Button</a>
+        <a className="btn flex items-center justify-center">Button</a>
       </div>
     </div>
   );
