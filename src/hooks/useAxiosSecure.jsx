01abce7a4eb6f32666dddd921c357549121ed38b
kaiyumdev <afrankaiyum34@gmail.com>
2024-09-10 01:50:35 +0600

    feat(useAxiosSecure): updated useAxiosSecure

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -19,8 +19,6 @@ const useAxiosSecure = () => {
       return config;
     },
     function (error) {
-      // Do something with request error
-
       return Promise.reject(error);
     }
   );
@@ -28,13 +26,9 @@ const useAxiosSecure = () => {
   //intercepts 401 and 403 status
   axiosSecure.interceptors.response.use(
     function (response) {
-      // Any status code that lie within the range of 2xx cause this function to trigger
-      // Do something with response data
       return response;
     },
     async (error) => {
-      // Any status codes that falls outside the range of 2xx cause this function to trigger
-      // Do something with response error
       const status = error?.response?.status;
       console.log("status error in the interceptor: ", error);
       if (status === 401 || status === 403) {
