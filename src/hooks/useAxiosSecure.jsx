a502f7cf54c07b0c0dfcc67f02fb8a2cf012024b
kaiyumdev <afrankaiyum34@gmail.com>
2024-09-09 19:17:22 +0600

    Feat(interceptor): implements interceptors for response & request method successfully

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -18,6 +18,21 @@ const useAxiosSecure = () => {
       return Promise.reject(error);
     }
   );
+
+  //intercepts 401 and 403 status
+  axiosSecure.interceptors.response.use(
+    function (response) {
+      // Any status code that lie within the range of 2xx cause this function to trigger
+      // Do something with response data
+      return response;
+    },
+    function (error) {
+      // Any status codes that falls outside the range of 2xx cause this function to trigger
+      // Do something with response error
+      console.log("status error in the interceptor: ", error);
+      return Promise.reject(error);
+    }
+  );
   return axiosSecure;
 };
 
