3d587a83d5859309726d31807734b230c6246bb3
kaiyumdev <afrankaiyum34@gmail.com>
2024-09-11 01:42:26 +0600

    feat(manageItems): show menuItems at manageItems page & completed design manageItems page successfully

--- src/Pages/Dashboard/ManageItems/ManageItems.jsx
+++ src/Pages/Dashboard/ManageItems/ManageItems.jsx
@@ -1,12 +1,65 @@
+import { FaEdit, FaTrashAlt } from "react-icons/fa";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import { Link } from "react-router-dom";
+import useMenu from "../../../hooks/useMenu";
 
 const ManageItems = () => {
+  const [menu] = useMenu();
   return (
     <div>
       <SectionTitle
         heading="Manage All Items"
         subHeading="Hurry Up"
       ></SectionTitle>
+      <div>
+        <div className="overflow-x-auto">
+          <table className="table w-full">
+            {/* head */}
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Image</th>
+                <th>Item Name</th>
+                <th>Price</th>
+                <th>Update</th>
+                <th>Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {menu.map((item, index) => (
+                <tr key={item._id}>
+                  <th>{index + 1}</th>
+                  <td>
+                    <img
+                      src={item.image}
+                      alt={item.name}
+                      width="50"
+                      height="50"
+                    />
+                  </td>
+                  <td>{item.name}</td>
+                  <td>{item.price}</td>
+                  <td>
+                    <Link to={`/dashboard/updateItem/${item._id}`}>
+                      <button className="btn btn-ghost btn-md bg-orange-500">
+                        <FaEdit></FaEdit>
+                      </button>
+                    </Link>
+                  </td>
+                  <td>
+                    <button
+                      //   onClick={() => handleDeleteItem(item)}
+                      className="btn btn-ghost btn-md bg-orange-500"
+                    >
+                      <FaTrashAlt></FaTrashAlt>
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 };
