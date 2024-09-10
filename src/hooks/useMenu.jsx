671298a372d46a439930d0aa8004db68a3fc30af
kaiyumdev <afrankaiyum34@gmail.com>
2024-09-10 22:56:08 +0600

    feat(menu): create useMenu to get menu data

--- src/hooks/useMenu.jsx
+++ src/hooks/useMenu.jsx
@@ -1,16 +1,30 @@
 /* eslint-disable no-unused-vars */
+import { useQuery } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
+import useAxiosPublic from "./useAxiosPublic";
 
 const useMenu = () => {
-  const [menu, setMenu] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  useEffect(() => {
-    fetch("http://localhost:5001/menu")
-      .then((res) => res.json())
-      .then((data) => setMenu(data));
-  }, []);
-  return [menu, loading, error];
+  // const [menu, setMenu] = useState([]);
+  // const [loading, setLoading] = useState(true);
+  // const [error, setError] = useState(null);
+  // useEffect(() => {
+  //   fetch("http://localhost:5001/menu")
+  //     .then((res) => res.json())
+  //     .then((data) => setMenu(data));
+  // }, []);
+  const axiosPublic = useAxiosPublic();
+  const {
+    data: menu = [],
+    isPending: loading,
+    refetch,
+  } = useQuery({
+    queryKey: ["menu"],
+    queryFn: async () => {
+      const res = await axiosPublic.get("/menu");
+      return res.data;
+    },
+  });
+  return [menu, loading, refetch];
 };
 
 export default useMenu;
