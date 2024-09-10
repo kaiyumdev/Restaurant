e1d6d3c34dfdc64b7a240261eba1eb6671c1bf0d
kaiyumdev <afrankaiyum34@gmail.com>
2024-09-10 15:41:47 +0600

    feat(image): get image hosting key & call image hosting api

--- src/Pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/Pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,7 +1,11 @@
+/* eslint-disable no-unused-vars */
 import { FaTrashAlt, FaUsers } from "react-icons/fa";
 import useUsers from "../../../hooks/useUsers";
 import Swal from "sweetalert2";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
+
+const image_hosting_key = import.meta.env.VITE_IMAGE_HOSING_KEY;
+const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
 
 const AllUsers = () => {
   const [users, refetch] = useUsers();
