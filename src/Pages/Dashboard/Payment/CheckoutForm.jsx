d7fa7dfcca38ca9c83c9ff65cbf63efb15c4c348
kaiyumdev <afrankaiyum34@gmail.com>
2024-09-11 11:17:15 +0600

    feat(stripe): intregate stripe at checkoutForm file

--- src/Pages/Dashboard/Payment/CheckoutForm.jsx
+++ src/Pages/Dashboard/Payment/CheckoutForm.jsx
@@ -1,5 +1,43 @@
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+
 const CheckoutForm = () => {
-  return <div>CheckoutForm</div>;
+  const stripe = useStripe();
+  const elements = useElements();
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (!stripe || !elements) {
+      return;
+    }
+    const card = elements.getElement(CardElement);
+
+    if (card == null) {
+      return;
+    }
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <CardElement
+        options={{
+          style: {
+            base: {
+              fontSize: "16px",
+              color: "#424770",
+              "::placeholder": {
+                color: "#aab7c4",
+              },
+            },
+            invalid: {
+              color: "#9e2146",
+            },
+          },
+        }}
+      />
+      <button type="submit" disabled={!stripe}>
+        Pay
+      </button>
+    </form>
+  );
 };
 
 export default CheckoutForm;
