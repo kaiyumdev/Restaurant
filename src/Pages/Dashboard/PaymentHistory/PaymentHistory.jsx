0e6dc8b5ca0c6fc133b43c21940d54d437332e97
kaiyumdev <afrankaiyum34@gmail.com>
2024-09-12 16:33:48 +0600

    feat(paymentHistory): create payments history page to show payments history data

--- src/Pages/Dashboard/PaymentHistory/PaymentHistory.jsx
+++ src/Pages/Dashboard/PaymentHistory/PaymentHistory.jsx
@@ -13,7 +13,34 @@ const PaymentHistory = () => {
       return res.data;
     },
   });
-  return <div></div>;
+  return (
+    <div>
+      <h1 className="text-3xl">Total Payments: {payments.length}</h1>
+      <div className="overflow-x-auto">
+        <table className="table table-zebra">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Price</th>
+              <th>Transaction Id</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {payments.map((payment, index) => (
+              <tr key={payment._id}>
+                <th>{index + 1}</th>
+                <td>{payment.price}</td>
+                <td>{payment.transactionId}</td>
+                <td>{payment.status}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default PaymentHistory;
