2ac9f46b68d7a65ebc898f95a08c4124fce5c237
kaiyumdev <afrankaiyum34@gmail.com>
2024-09-12 12:20:23 +0600

    feat(paymentHistory): create payment history routes

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -170,9 +170,9 @@ const Dashboard = () => {
                 </NavLink>
               </li>
               <li>
-                <NavLink to="/dashboard/bookings">
+                <NavLink to="/dashboard/paymentHistory">
                   <FaList></FaList>
-                  My Bookings
+                  Payment History
                 </NavLink>
               </li>
             </>
