ab081302693546a0283526c986c4e2785701fab4
kaiyumdev <afrankaiyum34@gmail.com>
2024-09-14 11:13:00 +0600

    feat(adminHome): fix the issue successfully to get all the data of admin-stats

--- src/Pages/Dashboard/AdminHome/AdminHome.jsx
+++ src/Pages/Dashboard/AdminHome/AdminHome.jsx
@@ -1,66 +1,61 @@
-// import { useQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import useAuth from "../../../hooks/useAuth";
-// import useAxiosSecure from "../../../hooks/useAxiosSecure";
-// import { FaBook, FaDollarSign, FaUsers } from "react-icons/fa";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { FaBook, FaDollarSign, FaUsers } from "react-icons/fa";
 
 const AdminHome = () => {
   const { user } = useAuth();
-  // const axiosSecure = useAxiosSecure();
-  // const { data: stats } = useQuery({
-  //   queryKey: ["admin-stats"],
-  //   queryFn: async () => {
-  //     const res = await axiosSecure.get("admin-stats");
-  //     return res.data;
-  //   },
-  // });
+  const axiosSecure = useAxiosSecure();
+  const { data: stats } = useQuery({
+    queryKey: ["admin-stats"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/admin-stats");
+      return res.data;
+    },
+  });
+  console.log(stats);
   return (
     <div>
       <h2 className="text-3xl">
-        <span>Hi, Welcome </span>
-        {user?.displayName ? user?.displayName : "Back"}
+        <span>Hi, Welcome</span> {user?.displayName ? user.displayName : "Back"}
       </h2>
+      <div className="stats shadow">
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <FaDollarSign className="text-3xl"></FaDollarSign>
+          </div>
+          <div className="stat-title">Revenue</div>
+          <div className="stat-value">${stats?.revenue}</div>
+          <div className="stat-desc">Jan 1st - Feb 1st</div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <FaUsers className="text-3xl"></FaUsers>
+          </div>
+          <div className="stat-title">Users</div>
+          <div className="stat-value">{stats?.users}</div>
+          <div className="stat-desc">↗︎ 400 (22%)</div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <FaBook className="text-3xl"></FaBook>
+          </div>
+          <div className="stat-title">Menu Items</div>
+          <div className="stat-value">{stats?.menuItems}</div>
+          <div className="stat-desc">↘︎ 90 (14%)</div>
+        </div>
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <FaBook className="text-3xl"></FaBook>
+          </div>
+          <div className="stat-title">Orders</div>
+          <div className="stat-value">{stats?.orders}</div>
+          <div className="stat-desc">↘︎ 90 (14%)</div>
+        </div>
+      </div>
     </div>
-    // <div>
-    //   <h2 className="text-3xl">
-    //     <spna>Hi, Welcome</spna> {user?.displayName ? user.displayName : "Back"}
-    //   </h2>
-    //   <div className="stats shadow">
-    //     <div className="stat">
-    //       <div className="stat-figure text-secondary">
-    //         <FaDollarSign className="text-3xl"></FaDollarSign>
-    //       </div>
-    //       <div className="stat-title">Revenue</div>
-    //       <div className="stat-value">${stats?.revenue}</div>
-    //       <div className="stat-desc">Jan 1st - Feb 1st</div>
-    //     </div>
-
-    //     <div className="stat">
-    //       <div className="stat-figure text-secondary">
-    //         <FaUsers className="text-3xl"></FaUsers>
-    //       </div>
-    //       <div className="stat-title">Users</div>
-    //       <div className="stat-value">{stats?.users}</div>
-    //       <div className="stat-desc">↗︎ 400 (22%)</div>
-    //     </div>
-
-    //     <div className="stat">
-    //       <div className="stat-figure text-secondary">
-    //         <FaBook className="text-3xl"></FaBook>
-    //       </div>
-    //       <div className="stat-title">Menu Items</div>
-    //       <div className="stat-value">{stats?.menuItems}</div>
-    //       <div className="stat-desc">↘︎ 90 (14%)</div>
-    //     </div>
-    //     <div className="stat">
-    //       <div className="stat-figure text-secondary">
-    //         <FaBook className="text-3xl"></FaBook>
-    //       </div>
-    //       <div className="stat-title">Orders</div>
-    //       <div className="stat-value">{stats?.orders}</div>
-    //       <div className="stat-desc">↘︎ 90 (14%)</div>
-    //     </div>
-    //   </div>
-    // </div>
   );
 };
 
