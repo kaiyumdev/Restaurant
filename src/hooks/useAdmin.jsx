ce7abdf83b2708b1dd9547c9fa6db1276c97050d
kaiyumdev <afrankaiyum34@gmail.com>
2024-09-14 09:52:02 +0600

    feat(fix): fix the issue successfully

--- src/hooks/useAdmin.jsx
+++ src/hooks/useAdmin.jsx
@@ -3,12 +3,14 @@ import useAxiosSecure from "./useAxiosSecure";
 import useAuth from "./useAuth";
 
 const useAdmin = () => {
-  const { user } = useAuth();
+  const { user, loading } = useAuth();
   const axiosSecure = useAxiosSecure();
   const { data: isAdmin = [], isPending: isAdminLoading } = useQuery({
     queryKey: [user?.email, "isAdmin"],
+    enabled: !loading,
     queryFn: async () => {
-      const res = await axiosSecure.get(`/users/admin/${user.email}`);
+      const res = await axiosSecure.get(`/users/admin/${user?.email}`);
+      console.log(res);
       return res.data?.admin;
     },
   });
